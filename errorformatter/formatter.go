06ec0e30787a33e718c5be967839684022a68f4d
David Fornazier <david.fornazier@gmail.com>
2021-11-11 12:10:36 -0300

    v0.1

---
+++ errorformatter/formatter.go
@@ -0,0 +1,35 @@
+package errorformatter
+
+import "github.com/go-playground/validator/v10"
+
+type StructError struct {
+	Field string
+	Tag   string
+}
+
+func FormatError(e error) []StructError {
+	var formatter []StructError
+
+	for _, err := range e.(validator.ValidationErrors) {
+		ex := StructError{
+			Field: err.Field(),
+			Tag:   translateTag(err.Tag()),
+		}
+		formatter = append(formatter, ex)
+	}
+
+	return formatter
+}
+
+func translateTag(tag string) string {
+	switch tag {
+	case "lte":
+		return "Less than"
+	case "gte":
+		return "Greater than"
+	case "email":
+		return "E-mail"
+	default:
+		return tag
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"playingwitherrors/errorformatter"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type User struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Age      int    `json:"age" validate:"required,gte=0,lte=100"`
+	Birthday string `json:"birthday" validate:"required,datetime=2006-01-02"`
+}
 
 func main() {
-	fmt.Println("start")
+	validate := validator.New()
+
+	withError := User{
+		Name:     "david",
+		Email:    "david@email,com",
+		Age:      103,
+		Birthday: "1999-11-02",
+	}
+	err := validate.Struct(withError)
+	if err != nil {
+		formated := errorformatter.FormatError(err)
+		fmt.Printf("Found:\n%+v\n", formated)
+	}
+
 }
