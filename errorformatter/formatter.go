9b0bda83790853fd7c8a30f9929a80e79e938c81
David Fornazier <david.fornazier@gmail.com>
2021-11-12 01:16:58 -0300

    init cpf validator

--- errorformatter/formatter.go
+++ errorformatter/formatter.go
@@ -1,10 +1,36 @@
 package errorformatter
 
-import "github.com/go-playground/validator/v10"
+import (
+	"sync"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type StructError struct {
 	Field string
 	Tag   string
+}
+
+var lock = &sync.Mutex{}
+var Validador *validator.Validate
+
+func InitValidador() *validator.Validate {
+	if Validador == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		Validador = validator.New()
+		Validador.RegisterValidation("cpf", cpfValidator)
+	}
+
+	return Validador
+}
+
+func cpfValidator(f validator.FieldLevel) bool {
+	if f.Field().IsNil() {
+		return false
+	}
+
+	return false
 }
 
 func FormatError(e error) []StructError {
